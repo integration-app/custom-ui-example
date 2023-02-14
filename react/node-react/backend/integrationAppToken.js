9660b73f14b9558d22b894b65631dd3fee11f6f8
Vladyslav Ursul <vlad@integration.app>
2023-02-14 18:15:17 +0100

    first commit

---
+++ react/node-react/backend/integrationAppToken.js
@@ -0,0 +1,37 @@
+const express = require('express')
+const jwt = require('jsonwebtoken')
+const cors = require('cors')
+
+const app = express()
+app.use(cors())
+
+app.get('/integration-token', (req, res) => {
+
+    const tokenData = {
+        // Identifier of user or organization.
+        id: 'john.doe@email.com',
+        // Human-readable name (it will simplify troubleshooting)
+        name: 'John Doe',
+        // (optional) Any user fields you want to attach to your user.
+        fields: {
+            userField: '<user fields value>'
+        }
+    }
+
+    // Your workspace key and secret.
+    // You can find them on the Settings page.
+    const WORKSPACE_KEY = ''
+    const WORKSPACE_SECRET = ''
+    const options = {
+        issuer: WORKSPACE_KEY,
+        // To prevent token from being used for too long
+        expiresIn: 7200,
+    }
+
+    const token = jwt.sign(tokenData, WORKSPACE_SECRET, options)
+
+    res.send(JSON.stringify(token))
+})
+
+app.listen(8080)
+
